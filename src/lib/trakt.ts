c2c09c342bdbf75a9dfd7914ad063a576da9c512
PacerguyDon <PacerguyDon@users.noreply.github.com>
2026-04-20 16:46:55 -0600

    Update trakt.ts

--- src/lib/trakt.ts
+++ src/lib/trakt.ts
@@ -1,13 +1,13 @@
-import type { TraktContext } from '../types'
+import type { TraktContext, TraktList, TraktListItem } from '../types'
 
 export async function fetchTraktContext(): Promise<TraktContext | null> {
   return null;
 }
 
-export async function fetchListsForUser() {
+export async function fetchListsForUser(_user: string): Promise<TraktList[]> {
   return [];
 }
 
-export async function fetchListItemsForUser() {
+export async function fetchListItemsForUser(_args: { user: string; slug: string }): Promise<TraktListItem[]> {
   return [];
 }
