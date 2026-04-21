83428d6fab1acbecf985196a17714b550dadea57
PacerguyDon <PacerguyDon@users.noreply.github.com>
2026-04-21 13:38:17 -0600

    line 17 rewrite

--- src/lib/trakt.ts
+++ src/lib/trakt.ts
@@ -14,10 +14,9 @@ export function fetchListsForUser(user: string, signal?: AbortSignal) {
   return fetchJson<TraktList[]>(`/api/trakt/users/${encodeURIComponent(user)}/lists`, signal);
 }
 
-export function fetchListItemsForUser({ user, slug, signal }: { user: string; slug: string; signal?: AbortSignal; }) {
+export function fetchListItemsForUser(user: string, slug: string, signal?: AbortSignal) {
   return fetchJson<TraktListItem[]>(`/api/trakt/users/${encodeURIComponent(user)}/lists/${encodeURIComponent(slug)}/items`, signal);
 }
-
 
 export const toPosterProxyUrl: any = (url: string | null) => {
   return url ?? '';
