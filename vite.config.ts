25c910c311b1bac90890eed39e4c0f826a6497b5
PacerguyDon <PacerguyDon@users.noreply.github.com>
2026-04-21 14:56:00 -0600

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This fix solves the 404 by pointing to your specific repo folder
+  base: '/betterer-covers/', 
   server: {
     proxy: {
       '/api': 'http://localhost:8787',
